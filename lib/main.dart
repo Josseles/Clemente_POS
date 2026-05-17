884ad58f3340ddf619c17dbbff70db9fd7ae5b06
Josseles <josselesuadeo@gmail.com>
2026-05-17 01:03:13 -0700

    -Desplace el uso de la carpeta Database a Data/Database

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:clemente_pos/database/database_helper.dart';
+//import 'package:clemente_pos/database/database_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
